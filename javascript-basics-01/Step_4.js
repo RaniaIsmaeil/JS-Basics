06b07afe51307272a6a66d0a45e9a9bdd4bdb968
RaniaIsmaeil <ranyasmaelgmail.com>
2021-05-28 23:27:24 +0300

    step-4

--- javascript-basics-01/Step_4.js
+++ javascript-basics-01/Step_4.js
@@ -1,1 +1,4 @@
-var name = prompt ("What is you name?");
+var name = prompt ("What is your name?");
+var surname = prompt ("What is your surname?");
+var City = prompt ("Where do you live?");
+alert ("Hello, " + name );
