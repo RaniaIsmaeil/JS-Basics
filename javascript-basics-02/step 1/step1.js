9937bcc927d11a1f74db7555976ec8147755260d
RaniaIsmaeil <ranyasmaelgmail.com>
2021-05-30 16:51:08 +0300

    step1

---
+++ javascript-basics-02/step 1/step1.js
@@ -0,0 +1,14 @@
+
+var target = document.querySelector("#image1");
+target.addEventListener("mouseover", mOver, false);
+target.addEventListener("mouseout", mOut, false);
+
+function mOver() {
+   target.src = "images/image1_2.jpg";
+}
+
+function mOut() {  
+   target.src = "images/image1.jpg";
+}
+
+
