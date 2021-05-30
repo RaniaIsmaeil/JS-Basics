de331277929c436ac928d5a493a993e4085c1116
RaniaIsmaeil <ranyasmaelgmail.com>
2021-05-31 00:18:37 +0300

    step-2

---
+++ javascript-basics-03/step 2/step2.js
@@ -0,0 +1,14 @@
+
+function texts(anchor){
+    var x = document.getElementById("show");
+    var y =document.getElementById("hide");
+   
+    if(document.getElementById(anchor) == show){
+    document.getElementById("texte").style.display="block";
+     }
+    else{
+       if(document.getElementById(anchor) == hide){
+        document.getElementById("texte").style.display="none";
+    }
+  }
+}
