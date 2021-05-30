c5061210ab0ae6f61030dd47785f47a0822922a1
RaniaIsmaeil <ranyasmaelgmail.com>
2021-05-31 01:18:25 +0300

    step-3

---
+++ javascript-basics-03/step 3/step3.js
@@ -0,0 +1,13 @@
+var display = document.getElementById("text");
+function displayGreen()
+{
+text.style.color="green";
+}
+function displayRed()
+{
+text.style.color="red";
+}
+function displayBlue()
+{
+text.style.color="blue";
+}
