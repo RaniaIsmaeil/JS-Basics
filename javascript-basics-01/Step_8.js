8aa83f6ea34b965ddcbf9eb47e12a26ff36eef9c
RaniaIsmaeil <ranyasmaelgmail.com>
2021-05-30 16:12:43 +0300

    step-8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,12 @@
+
+var btn =document.getElementById("validate");
+btn.addEventListener("click",function ageExercise(age){
+    var age = document.getElementById("age").value;
+    if (age > 18){
+        alert ("You are over 18")
+    }
+    else{
+        alert ("You are under 18");
+    }
+})
+
