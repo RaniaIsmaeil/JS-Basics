e0a2825669b92077d48494ca944f660edec646d1
RaniaIsmaeil <ranyasmaelgmail.com>
2021-05-30 18:32:10 +0300

    step-2

---
+++ javascript-basics-02/step 2/step2.js
@@ -0,0 +1,11 @@
+
+// var input = document.querySelector("#name");
+// input.addEventListener("blur", blur)
+
+// function blur() {
+//     alert("thank you for participating!");
+// }
+
+function write() {
+    alert("thank you for participating!");
+}
