719b377df44ec41eed39bce77ec56965a0d3c261
RaniaIsmaeil <ranyasmaelgmail.com>
2021-05-31 01:49:03 +0300

    step-4

---
+++ javascript-basics-03/step 4/step4.js
@@ -0,0 +1,14 @@
+var confirm = document.querySelector("#confirmation");
+var button = document.querySelector("button");
+ 
+var change_border = function() {
+    var password = document.querySelector("#password");
+    var pass_value = password.value;
+    if(pass_value !== null ) {
+        password.style.border = "3px solid red";
+        confirm.style.border = "3px solid red";
+    }
+}
+
+button.addEventListener("click", change_border);
+
